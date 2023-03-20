62548bd5a4dd53cbb87998d2ec63ba07a3584fbd
mic-dem <michal.demski@interia.pl>
2023-03-20 22:45:04 +0100

    Change App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Header} from "./components/layout/Header";
 import {Main} from "./components/Main/Main";
+import {LoginLink} from "./components/layout/LoginLink";
+import {NickContext} from './context/nick.context';
 
 function App() {
+    const [nick, setNick] = useState('');
     return (
-        <div className="App">
-            <Header/>
-            <Main/>
-        </div>
+        <NickContext.Provider value={{nick, setNick}}>
+            <div className="App">
+                <LoginLink/>
+                <Header/>
+                <Main/>
+            </div>
+        </NickContext.Provider>
     );
 }
 
